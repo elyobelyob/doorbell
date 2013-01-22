e3d9470b71cfa86831247957667d4297f461823c
Nick Boyle <nick@elyob.com>
2013-01-22 09:20:27 +0000

    push time change

--- snapalert.php
+++ snapalert.php
@@ -7,7 +7,7 @@ $files = preg_grep('/^([^.])/', scandir($dir));
 
 foreach ($files as $filelist) {
 	if (filemtime($dir.$filelist) > time()-(5*60)) {
-		$event =  'event at '.date("Y-m-d h:i:s",time());
+		$event =  'event at '.date("Y-m-d H:i:s",time());
 
 		shell_exec("./findtouser.sh ".$prowlkey." 'Doorbell ' '$event' '' ".$prowlpriority." 'http://".$htaccesspwd."@".$url."/images/".date('Ymd')."/doorbell/".$filelist."' > /dev/null 2>/dev/null &");
 		exit;
