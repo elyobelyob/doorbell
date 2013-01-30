028a5cb2a121880ecb0d935d182f0532a55efaf5
Nick Boyle <nick@elyob.com>
2013-01-30 17:19:44 +0000

    Try to fix destinations

--- ajax-doorbell.php
+++ ajax-doorbell.php
@@ -15,14 +15,44 @@
 	<div data-role="content">
 		<ul class="gallery">
 
+
+
 <?php
-//error_reporting(0);
-set_time_limit(0);
+
+// At this point we will create any missing thumbs
+function createThumbs( $pathToImages, $pathToThumbs, $imageName, $thumbWidth )
+{
+        // parse path for the extension
+        $info = pathinfo($pathToImages . $imageName);
+        // continue only if this is a JPEG image
+        if ( strtolower($info['extension']) == 'jpg' )
+        {
+                // load image and get image size
+                $img = imagecreatefromjpeg( "{$pathToImages}{$imageName}" );
+                $width = imagesx( $img );
+                $height = imagesy( $img );
+
+                // calculate thumbnail size
+                $new_width = $thumbWidth;
+                $new_height = floor( $height * ( $thumbWidth / $width ) );
+
+                // create a new temporary image
+                $tmp_img = imagecreatetruecolor( $new_width, $new_height );
+
+                // copy and resize old image into new image
+                imagecopyresized( $tmp_img, $img, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
+
+                // save thumbnail into a file
+                imagejpeg( $tmp_img, $pathToThumbs.$imageName );
+                //unlink($img);
+        }
+}
+
 date_default_timezone_set('Europe/London');
 
 require_once('settings.php');
 
-$basedir = $photopath;
+$basedir = '/media/external_hdd';
 
 if (!empty($_GET['mydate'])) {
 	$reqdate = date('Ymd', strtotime($_GET['mydate']));
@@ -30,18 +60,40 @@ if (!empty($_GET['mydate'])) {
 	$reqdate = date('Ymd');
 }
 
-$localdir = "/images/".$reqdate."/doorbell/";
+$localdir = $basedir."/images/".$reqdate."/doorbell/";
+$destinationThumbs = '/images/'.$reqdate.'/thumbs/';
 $urldir = "http://".$url."/images/".$reqdate."/doorbell/";
 $urlthumbdir = "http://".$url."/images/".$reqdate."/thumbs/";
+$i = 0;
 
 // Identify directories
 $source = $basedir.$localdir;
-$i = 0;
 
 // Get array of all source files
-if (is_dir($source)) {
-	$files = scandir($source,1);
+if (is_dir($localdir)) {
+	$files = scandir($localdir,1);
 }
+
+// Create Thumbs folder
+if (!file_exists($destinationThumbs)) {
+    mkdir($destinationThumbs, 0777);
+}
+
+// parse each file
+foreach ($files as $file) {
+
+    // make sure it's not a base 'file'
+	if (in_array($file, array(".","..","thumbs"))) { continue; }
+
+	// if already exists in thumbs, ignore
+	if (file_exists($destinationThumbs.$file)) { continue; }
+
+    // Create Thumb	
+	createThumbs($localdir,$destinationThumbs,$file,300);
+
+}
+
+// From here we generate Ajax output file
 
 // Setup slider from front page
 if (isset($_GET['slider'])) {
